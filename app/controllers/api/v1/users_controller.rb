0c735ee786cb51ae30c333875b69eb5396ab9723
waffe_x20 <argueta_eduardo@yahoo.de>
2022-08-17 21:18:03 -0600

    Adds update action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,23 +1,38 @@
 class Api::V1::UsersController < ApplicationController
-    # GET /users/1
-    def show
-        render json: User.find(params[:id])
-    end
+  before_action :set_user, only: %i[show update]
 
-    # POST /users
-    def create
-        @user = User.new(user_params)
+  # GET /users/1
+  def show
+    render json: @user
+  end
 
-        if @user.save
-            render json: @user, status: :created
-        else
-            render json: @user.errors, status: :unprocessable_entity
-        end
-    end
+  # POST /users
+  def create
+    @user = User.new(user_params)
 
-    private
-    # Only allow a trusted parameter "white list" through
-    def user_params
-        params.require(:user).permit(:email, :password)
+    if @user.save
+        render json: @user, status: :created
+    else
+        render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+        render json: @user, status: :ok
+    else
+        render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  # Only allow a trusted parameter "white list" through
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
